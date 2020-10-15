ed51e6ca1c77723c4faa11a70d79cf1629e9e66e
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-10-15 11:18:47 +0900

    改定表を読み込む処理を追加

--- PriceRevision.py
+++ PriceRevision.py
@@ -1,5 +1,6 @@
 import numpy as np
-from openpyxl import Workbook
+from openpyxl import Workbook,load_workbook
+import pandas as pd
 
 class PriceRevision():
     
@@ -20,5 +21,15 @@ class PriceRevision():
 
 
     def read_master_csv(self,master_csv):
-        master_jan=np.genfromtxt(master_csv,delimiter=',',skip_header=1,usecols=0,encoding='CP932',dtype=str)
-        print(master_jan)
+        self.__master_jan=np.genfromtxt(master_csv,delimiter=',',skip_header=1,usecols=0,encoding='CP932',dtype=str)
+
+    def read_revision_sheet(self):
+        from itertools import islice
+
+        wb=load_workbook('revision.xlsx')
+        ws=wb['改定表貼り付けシート']
+        data=ws.values
+        data=list(data)
+        df=pd.DataFrame(data)
+        revision_arr=df.to_numpy()
+        
