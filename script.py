7a50c37c0e885b915cef62ac16effb3e9f27c6fd
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-10-15 17:31:37 +0900

    テスト

--- script.py
+++ script.py
@@ -2,5 +2,8 @@ from PriceRevision import PriceRevision
 
 pr=PriceRevision()
 pr.load_master('shop_datas/master.csv')
+#pr.create_revision_csv()
 pr.create_maker_list()
 pr.query_by_maker('菅公工業')
+pr.create_revision_data_frame('revision.xlsx')
+#pr.execute_search()
